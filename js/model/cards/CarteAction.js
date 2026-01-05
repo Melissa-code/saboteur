c0ec0fcc2b2fb6931b428c9447d2c5539814951b
Melissa Benard <melissa.benard@interieur.gouv.fr>
2026-01-05 15:33:37 +0100

    refactor: simplify CarteAction predicates and add JSDoc

--- js/model/cards/CarteAction.js
+++ js/model/cards/CarteAction.js
@@ -1,27 +1,42 @@
 import Card from './Card.js';
 import Actions from '../enums/Actions.js';
 
+/**
+ * Représente une carte d'effet (blocage, réparation, éboulement...)
+ * hérite de la classe abstraite Card
+ */
 class CarteAction extends Card {
+    /**
+     * @param {string} image - path de l'image de la carte
+     * @param {string} titreAction - type d'action issu de l'Enum Actions
+     */
     constructor(titreAction, image) {
         super(image);
         this.titreAction = titreAction; 
     }
 
-    // bool 
     estCarteBloquante() {
-        return this.titreAction == Actions.CASSER_CHARIOT 
-            || this.titreAction == Actions.CASSER_LAMPE
-            || this.titreAction == Actions.CASSER_PIOCHE
+        const cartesBloquantes = [
+            Actions.CASSER_CHARIOT,
+            Actions.CASSER_LAMPE,
+            Actions.CASSER_PIOCHE
+        ];
+        
+        return cartesBloquantes.includes(this.titreAction);
     }
 
     estCarteReparation() {
-        return this.titreAction == Actions.REPARER_CHARIOT
-            || this.titreAction == Actions.REPARER_LAMPE
-            || this.titreAction == Actions.REPARER_PIOCHE
-            || this.titreAction == Actions.REPARER_CHARIOT_LAMPE
-            || this.titreAction == Actions.REPARER_LAMPE_PIOCHE
-            || this.titreAction == Actions.REPARER_PIOCHE_CHARIOT
-    }
+        const cartesReparation = [
+            Actions.REPARER_CHARIOT,
+            Actions.REPARER_LAMPE,
+            Actions.REPARER_PIOCHE,
+            Actions.REPARER_CHARIOT_LAMPE,
+            Actions.REPARER_LAMPE_PIOCHE,
+            Actions.REPARER_PIOCHE_CHARIOT
+        ];
+
+        return cartesReparation.includes(this.titreAction);
+    }; 
 }
 
 export default CarteAction;
