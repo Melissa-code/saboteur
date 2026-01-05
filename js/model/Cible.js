346359c1845dd9486b6c1d4b7b476a0df0d6ac36
Melissa Benard <melissa.benard@interieur.gouv.fr>
2026-01-05 13:03:23 +0100

    docs: add documentation for Cible class

--- js/model/Cible.js
+++ js/model/Cible.js
@@ -1,16 +1,15 @@
-import TypesCibles from './TypesCibles.js';
-
 class Cible {
+    /**
+     * @param {string} type - le type de cible 
+     * @param {Array|null} reference - données de référence de la cible :
+     * - MATRICE => reference = [x, y] position dans la matrice
+     * - JOUEUR => reference = [numJoueur, numCarte] , num= -1 | num carte cliquee
+     * - CORBEILLE => reference = null
+     * - EXTERIEUR => reference = null
+     */
     constructor(type, reference) {
         this.type = type; 
         this.reference = reference; 
-        /*
-            MATRICE => reference = [x, y] position dans la matrice
-            JOUEUR => reference = [numJoueur, numCarte] , num= -1 | num carte cliquee
-            CORBEILLE => reference = null
-            EXTERIEUR => reference = null
-        */
-
     }
 }
 
